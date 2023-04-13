407b90c7e2ca634b852e274bfbd4aa153e7de267
krishhhhhhhhhhna <130647550+krishhhhhhhhhhna@users.noreply.github.com>
2023-04-13 14:59:55 +0530

    Update krish.java

--- krish.java
+++ krish.java
@@ -1,5 +1,13 @@
-public class software{
-public static void main(string args[]){
-System.out.println("krishna");
-}
+class krish{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a =10;
+        int b =20;
+        if(a==b){
+            System.out.print("it is a square");
+        }
+        else{
+            System.out.print(" is a rectangle");
+        }
+    }
 }
